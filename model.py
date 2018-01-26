f4ec33889f5c3147e9c028f8f0d5c4e445617177
Ali <aaalyahy@uci.edu>
2018-01-25 21:48:16 -0800

    finished model

--- model.py
+++ model.py
@@ -1,44 +1,78 @@
-import csv
-with open('ES.csv') as f:
-    reader = csv.reader(f)
-    testinguser = list(reader)
-
-password=[,,,,]
 
 
-time=len(testinguser)-1
-print(time)
+def difference_rating(infinger_print,passfinger_print):
+	time1=len(infinger_print)-1
+	time2=len(passfinger_print)-1
+	time=0
 
-rating=0
+	if time1<time2:
+		time=time1
+	else:
+		time=time2
+	
 
-theta=[]
-alpha=[]
-low_beta=[]
-high_beta=[]
-gamma=[]
+	rating=0
 
-
-for row in testinguser[1:]: #this is to change the input array from wave x time to time x wave
-    theta.append(row[1])
-    alpha.append(row[2])
-    low_beta.append(row[3])
-    high_beta.append(row[4])
-    gamma.append(row[5])
-
-brainwave=[theta,alpha,low_beta,high_beta,gamma]
+	in_sorted=m_inv(infinger_print)
+	pass_sorted=m_inv(passfinger_print)
 
 
-for wave in range(5):
-    for t in range(time):
-        rating+=testinguser[wave][t]-password[wave][t]
+	for wave in range(5):
+   		for t in range(time):
+       			rating+=float(in_sorted[wave][t])-float(pass_sorted[wave][t])
+	
+	return rating
 
 
+def avg_rating(infinger_print,passfinger_print):
+
+
+	rating=0
+
+	in_sorted=m_inv(infinger_print)
+	pass_sorted=m_inv(passfinger_print)
+	
+	avg_in=avgof_mat(in_sorted)
+	avg_pass=avgof_mat(pass_sorted)
+	
+	rating=avg_in-avg_pass
+	
+	return rating
 
 
     
 
 
+def m_inv(in_mat):
 
+	theta=[]
+	alpha=[]
+	low_beta=[]
+	high_beta=[]
+	gamma=[]
+
+	for row in in_mat[1:]: #this is to change the input array from wave x time to time x wave
+		theta.append(row[1])
+   	 	alpha.append(row[2])
+  	  	low_beta.append(row[3])
+  		high_beta.append(row[4])
+  		gamma.append(row[5])
+
+	brainwave=[theta,alpha,low_beta,high_beta,gamma]
+
+	return brainwave
+
+
+def avgof_mat(in_mat):
+	avg_in=0
+	for i in in_mat:
+		tmp=0
+		for entry in i:
+			tmp+=float(entry)
+		tmp=tmp/len(i)
+		avg_in+=tmp
+
+	return avg_in
     
 
 
---
+++ test_m.py
@@ -0,0 +1,36 @@
+from model import *
+import csv
+path='/home/mrhaboon/teamTelepathy/test_graphs/'
+
+
+
+def open_csv(in_csv):
+	with open(path+in_csv,"rb") as f:
+    		reader = csv.reader(f)
+	    	user = list(reader)
+	return user
+
+ali=open_csv('Ali_raw_eeg.csv')
+jenny=open_csv('Jenny_raw_eeg.csv')
+angela=open_csv('Angela_raw_eeg.csv')
+will=open_csv('will_raw_eeg.csv')
+
+
+listofusers=[ali,jenny,angela,will]
+
+test=open_csv('Ali_2_raw_eeg.csv')
+
+tmp_rating=500
+index=0
+
+for i in listofusers:
+	tmp1=difference_rating(test,i)
+	if(abs(tmp_rating)>abs(tmp1)):
+		tmp_rating=tmp1
+		index=listofusers.index(i)
+		
+
+
+print(index)
+
+	
